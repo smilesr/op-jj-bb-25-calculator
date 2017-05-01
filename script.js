ba627b32699cd6a2ba703b36a7070e9096e15727
Robert Smiles Jr <smilesr120@gmail.com>
2017-04-30 22:06:09 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -1,133 +1,82 @@
-  var storeNumber = {};
-  function calculateIt(num1, num2, operationSign){
-    const operat = {
-      '+': function(num1, num2){
-        return num1 + num2;
-      }, 
-      '*': function(num1, num2){
-        return num1 * num2;
-      },
-      '-': function(num1, num2){
-        return num1 - num2;
-      },
-      '/': function( num1, num2 ){
-        return num1 / num2;
-      }
-    }
-
-    var x = operat[operationSign];
-    var y = 1;
-    return x(num1,num2);
-  }
-
-  // var aNumber = document.querySelectorAll('.number');
-  const keys = Array.from(document.querySelectorAll('.button'));
-  keys.forEach(key => key.addEventListener('click', function(e) {
-    // console.log("clicked");
-    identifyInputType(e);
-  }));
-
-  sessionStorage.setItem('firstNumber', null);
-  sessionStorage.setItem('secondNumber', null);
-  sessionStorage.setItem('sign', null);
-
-
-  function storeIt(num){
-    var currentNumber = sessionStorage.getItem('firstNumber');
-    if (JSON.parse(currentNumber) === null){
-      sessionStorage.setItem('firstNumber',num);
-
-    } else {
-      sessionStorage.setItem('firstNumber', currentNumber.concat(num))
-    }
-    currentNumber = sessionStorage.getItem('firstNumber')
-    return currentNumber;
-  }
-  function identifyInputType(e){
-    // if (e.toElement.firstChild.nodeType === 3){
-    //   const inputValue = e.toElement.parentElement.dataset.key
-    // } else {
-    // const inputValue = e.target.dataset.key;
-    // }
-    const inputValue = e.toElement.firstChild.nodeType === 3 ? e.toElement.parentElement.dataset.key : e.target.dataset.key;
-    console.log(inputValue);
-    const regEx = /\d/;
-    const regExSign = /[\+\-\*\/]/;
-    const regExEqual = /\=/;
-    const regExClear = /C/;
-    console.log(inputValue);
-    if (regEx.test(inputValue)){
-      var newNumber = storeIt(inputValue);
-      console.log("newNUmber=" + newNumber);
-      displayKey(newNumber);
-    }
-    if (regExSign.test(inputValue)){
-      addSecondNumber(inputValue);
-    }
-    if (regExEqual.test(inputValue)){
-      console.log('equals');
-      var firstValue = parseInt(sessionStorage.getItem('secondNumber'));
-      var secondValue = parseInt(sessionStorage.getItem('firstNumber'));
-      var operationSign = sessionStorage.getItem('sign');
-      var answer = calculateIt(firstValue,secondValue,operationSign);
-      var screenDisplay = document.getElementById('digits');
-      screenDisplay.innerHTML = inputValue;
-      displayKey(answer);
-      sessionStorage.clear();
-    }
-    if (regExClear.test(inputValue)){
-      console.log('clear');
-      sessionStorage.clear();
-      displayKey('');
+var storeNumber = {};
+function calculateIt(num1, num2, operationSign){
+  const operat = {
+    '+': function(num1, num2){
+      return num1 + num2;
+    }, 
+    '*': function(num1, num2){
+      return num1 * num2;
+    },
+    '-': function(num1, num2){
+      return num1 - num2;
+    },
+    '/': function( num1, num2 ){
+      return num1 / num2;
     }
   }
+  var x = operat[operationSign];
+  var y = 1;
+  return x(num1,num2);
+}
 
+const keys = Array.from(document.querySelectorAll('.button'));
+keys.forEach(key => key.addEventListener('click', function(e) {
+      identifyInputType(e);
+    }));
 
-  //   switch (inputValue) {
-  //     case input'Oranges':
-  //   console.log('Oranges are $0.59 a pound.');
-  //   break;
-  //   var y = parseInt(inputValue);
-  //   if (inputValue.test(regEx))
-  //   if (isNaN(y)){
-  //     displayKey(inputValue);
-  //   createNumber(numberValue);
-  // }
+sessionStorage.setItem('firstNumber', null);
+sessionStorage.setItem('secondNumber', null);
+sessionStorage.setItem('sign', null);
 
-  // function createCalculation(numberValue){
-// var audio = document.querySelector(`div[data-key="${e.keyCode}"]`);
-// var xxx = document.querySelector("[data-key]").value;
-  //   if (sessionStorage.getItem('firstNumber')){
-  //     var firstItem = sessionStorage.get('firstNumber');
-
-  //   } else {
-  //     sessionStorage.setItem = ('firstNumber',inputValue);
-  //   }
-  // }
-  // function createNumber(inputValue){
- 
-  //   if (inputValue.test(regEx)){
-  //     sessionStorage.setItem = ('firstNumber',inputValue);
-  //   }
-  // }
-  function displayKey(inputValue){
-    const screenDisplay = document.getElementById('digits');
+function storeIt(num){
+  var currentNumber = sessionStorage.getItem('firstNumber');
+  if (JSON.parse(currentNumber) === null){
+    sessionStorage.setItem('firstNumber',num);
+  } else {
+    sessionStorage.setItem('firstNumber', currentNumber.concat(num))
+  }
+  currentNumber = sessionStorage.getItem('firstNumber')
+  return currentNumber;
+}
+function identifyInputType(e){
+  const inputValue = e.toElement.firstChild.nodeType === 3 ? e.toElement.parentElement.dataset.key : e.target.dataset.key;
+  const regEx = /\d/;
+  const regExSign = /[\+\-\*\/]/;
+  const regExEqual = /\=/;
+  const regExClear = /C/;
+  if (regEx.test(inputValue)){
+    var newNumber = storeIt(inputValue);
+    displayKey(newNumber);
+  }
+  if (regExSign.test(inputValue)){
+    addSecondNumber(inputValue);
+  }
+  if (regExEqual.test(inputValue)){
+    var firstValue = parseInt(sessionStorage.getItem('secondNumber'));
+    var secondValue = parseInt(sessionStorage.getItem('firstNumber'));
+    var operationSign = sessionStorage.getItem('sign');
+    var answer = calculateIt(firstValue,secondValue,operationSign);
+    var screenDisplay = document.getElementById('digits');
     screenDisplay.innerHTML = inputValue;
+    displayKey(answer);
+    sessionStorage.clear();
   }
-
-  function addSecondNumber(sign){
-    var temp = sessionStorage.getItem('firstNumber');
-    sessionStorage.setItem('secondNumber', temp);
-    sessionStorage.setItem('firstNumber', null);
-    sessionStorage.setItem('sign', sign);
+  if (regExClear.test(inputValue)){
+    sessionStorage.clear();
+    displayKey('');
   }
+}
 
+function displayKey(inputValue){
+  const screenDisplay = document.getElementById('digits');
+  screenDisplay.innerHTML = inputValue;
+}
 
-  // function runIt(num1, num2, oper){
+function addSecondNumber(sign){
+  var temp = sessionStorage.getItem('firstNumber');
+  sessionStorage.setItem('secondNumber', temp);
+  sessionStorage.setItem('firstNumber', null);
+  sessionStorage.setItem('sign', sign);
+}
 
-  //   return operat[oper](num1,num2);
-  // }
-  // var answer = runIt(4,4,'/');
-  // console.log(answer);
-  // console.log(Object.keys(operat))
 
